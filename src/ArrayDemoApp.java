bcae90a58d4658d445a9988647c4237c6fd09a2d
Cinda <cindarella0@hotmail.com>
2021-03-08 15:29:03 +0100

    arangement

---
+++ src/ArrayDemoApp.java
@@ -0,0 +1,25 @@
+public class ArrayDemoApp {
+    public static void main(String[] args) {
+
+        /*
+        datatype [] name = new datatype [length];
+        name[positionname 0...length] = values of datatype
+
+         */
+
+
+
+        int [ ] intArray = new int [5];  // statt zu benennen a, b, c, d, e, || int z1, z2, z3, z4, z5;
+        intArray[0] = 1;
+        intArray[1] = 45;
+        intArray[2] = 36;
+        intArray[3] = 9;
+        intArray[4] = 99; // für den 5. Wert
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(intArray[i] + ", ");
+
+        }
+
+    }
+}
---
+++ src/SortDemoApp.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class SortDemoApp {
+    public static void main(String[] args) {
+
+        int[] arr = new int[7];
+        arr [0] = 9;
+        arr [1] = 8;
+        arr [2] = 7;
+        arr [3] = 6;
+        arr [4] = 5;
+        arr [5] = 4;
+        arr [6] = 3;
+        arrangeOrder(arr);
+
+        /*int[] arr2 = {54, 45, 33, 12, 77, 21, 99};
+        arrangeOrder(arr2);
+        Arrays.toString(arr) ist das gleiche wie:
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+        } */
+        System.out.println();
+        System.out.println(Arrays.toString(arr));
+
+        //for ()
+
+        for (int i = 0; i < arr.length -1; i++) {
+            if (arr [i] > arr[i+1]){
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+        }
+
+
+    public static void arrangeOrder(int[] numbers) {
+        System.out.println("lenght = " + numbers.length);
+
+        int temp = numbers[1];
+        numbers[1] = numbers[0];
+        numbers[0] = temp;
+    }
+
+
+}
