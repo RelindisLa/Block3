1cd1f5a3f303ca9d4b8ff60327259aa42f0ecd3f
Cinda <cindarella0@hotmail.com>
2021-03-22 12:40:29 +0100

    Übungen

--- src/Übungen_3/ExerciseNumbers2.java
+++ src/Übungen_3/ExerciseNumbers2.java
@@ -30,20 +30,21 @@ public class ExerciseNumbers2 {
     }
 
 
-    public static int[] findDoubleNumbers(int[] arr) {
-        Arrays.sort(arr);
+    public static int[] findDoubleNumbers(int[] numbers) {
+        Arrays.sort(numbers);
         int counter = 0;
 
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            int keep = arr[i];
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] == keep) {
-                    System.out.println(arr[j]);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int keep = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] == keep) {
+                    System.out.println(numbers[j]);
+                    keep = numbers[i];
                 }
             }
         }
-        return arr;
+        return numbers;
     }
 }
 
