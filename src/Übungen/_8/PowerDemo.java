db1664adbb0292a9ebc0514ff56be6fa775aa613
Cinda <cindarella0@hotmail.com>
2021-03-23 15:46:42 +0100

    PowerDemo

---
+++ src/Übungen/_8/PowerDemo.java
@@ -0,0 +1,49 @@
+package Übungen._8;
+
+public class PowerDemo {
+
+    public static void main(String[] args) {
+        int x = 0;
+        int n = 0;
+
+
+        System.out.println(powerTimesN(3,0));
+        System.out.println(powerTimesN(3,1));
+        System.out.println(powerTimesN(3,3));
+        System.out.println(powerTimesN(2,3));
+        System.out.println(powerTimesN(2,4));
+        System.out.println();
+        System.out.println(powerSimple(3,0));
+        System.out.println(powerSimple(3,1));
+        System.out.println(powerSimple(3,3));
+        System.out.println(powerSimple(2,3));
+        System.out.println(powerSimple(2,4));
+
+
+    }
+
+    public static int powerTimesN (int x, int n) {
+        int value = 1;
+        for (int i = 0; i < n; i++){
+            value = value * x;
+        }
+        return value;
+    }
+
+    public static int powerSimple (int x, int n){
+
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
+        if (n % 2 == 0)
+           return powerSimple(x, n/2) * powerSimple(x, n/2);
+        else
+            return x * powerSimple(x, n/2) * powerSimple(x, n/2);
+    }
+
+
+
+
+
+}
