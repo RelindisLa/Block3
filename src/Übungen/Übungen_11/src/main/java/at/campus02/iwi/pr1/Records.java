9ceb1556b9b92c94e7dce82baf685d2f19a12873
Cinda <cindarella0@hotmail.com>
2021-03-25 14:26:45 +0100

    DJ Jane

--- src/Übungen/Übungen_11/src/main/java/at/campus02/iwi/pr1/Records.java
+++ src/Übungen/Übungen_11/src/main/java/at/campus02/iwi/pr1/Records.java
@@ -4,22 +4,59 @@ public class Records {
     static final double LIMIT_SINGLE = 20.0;
 
     public static void main(String[] args) {
-        final int[]    GENRES  = RecordsData.GENRES;
-        final String[] ARTISTS = RecordsData.ARTISTS;
-        final double[] LENGTHS = RecordsData.LENGTHS; // in minuten
+        final int[]    GENRES  = at.campus02.iwi.pr1.RecordsData.GENRES;
+        final String[] ARTISTS = at.campus02.iwi.pr1.RecordsData.ARTISTS;
+        final double[] LENGTHS = at.campus02.iwi.pr1.RecordsData.LENGTHS;
+
+
+        System.out.println("ist es eine Singel? " + istSingle(LENGTHS,1));
+        System.out.println(GENRES.length);
+        System.out.println(ARTISTS.length);
+        System.out.println(LENGTHS.length);
+        System.out.println();
+        System.out.println(plattenVonInterpret(ARTISTS, "Nirvana"));
+        System.out.println(plattenVonInterpret(ARTISTS, "Kroko Jack"));
+        System.out.println(nimmInterpret(ARTISTS, "Nirvana"));
+        System.out.println(nimmInterpret(ARTISTS, "Kroko Jack"));
+        nimmInterpret(ARTISTS,"Blub");
+
+
+
+        // in minuten
     }
 
     public static boolean istSingle(double[] lengths, int index) {
-        return false;
+        if (lengths[index] > 20.0)
+            return false;
+        else
+            return true;
     }
 
     public static int plattenVonInterpret(String[] artists, String artist) {
-        return 0;
+        int counter = 0;
+        for (int i = 0; i < artists.length; i++) {
+            if (artist.equals(artists[i])){
+                counter++;
+            }
+        }
+        return counter;
     }
 
     public static int[] nimmInterpret(String[] artists, String artist) {
-        return null;
+        int[] allePlatten = new int[plattenVonInterpret(artists, artist)];
+        int index = 0;
+            for (int i = 0; i < artists.length; i++) {
+                if (artists[i].equals(artist)){
+                    //artist.equals(artists[i])
+                    //  ist artist an Stelle Artist/Pos.i gleich artist, wenn ja, dann
+                    allePlatten[index] = i;
+                    index++;
+                }
+                System.out.println(index);
+            }
+        return allePlatten[];
     }
+
 
     public static double mittlereDauerAlbum(double[] lengths) {
         return 0.0;
--- src/Übungen/Übungen_11/src/test/java/at/campus02/iwi/pr1/Pr1Bsp1Test.java
+++ src/Übungen/Übungen_11/src/test/java/at/campus02/iwi/pr1/Pr1Bsp1Test.java
@@ -14,29 +14,29 @@ public class Pr1Bsp1Test {
 
     @Before
     public void setup(){
-        GENRES = RecordsData.GENRES;
-        ARTISTS = RecordsData.ARTISTS;
-        LENGTHS = RecordsData.LENGTHS;
+        GENRES = at.campus02.iwi.pr1.RecordsData.GENRES;
+        ARTISTS = at.campus02.iwi.pr1.RecordsData.ARTISTS;
+        LENGTHS = at.campus02.iwi.pr1.RecordsData.LENGTHS;
     }
 
     @Test
     public void testIstSingle() {
-        assertTrue(Records.istSingle(LENGTHS, 3));
-        assertTrue(Records.istSingle(LENGTHS, 4));
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 3));
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 4));
     }
 
     @Test
     public void testPlattenVonInterpret() {
-        int nirvana = Records.plattenVonInterpret(ARTISTS, "Nirvana");
-        int kroko_jack = Records.plattenVonInterpret(ARTISTS, "Kroko Jack");
+        int nirvana = at.campus02.iwi.pr1.Records.plattenVonInterpret(ARTISTS, "Nirvana");
+        int kroko_jack = at.campus02.iwi.pr1.Records.plattenVonInterpret(ARTISTS, "Kroko Jack");
         assertEquals(nirvana, 8);
         assertEquals(kroko_jack, 0);
     }
 
     @Test
     public void testNimmInterpret() {
-        int[] nirvana_alben = Records.nimmInterpret(ARTISTS, "Nirvana");
-        int[] kroko_jack_alben = Records.nimmInterpret(ARTISTS, "Kroko Jack");
+        int[] nirvana_alben = at.campus02.iwi.pr1.Records.nimmInterpret(ARTISTS, "Nirvana");
+        int[] kroko_jack_alben = at.campus02.iwi.pr1.Records.nimmInterpret(ARTISTS, "Kroko Jack");
 
         int[] nirvana_soll = new int[]{37, 51, 63, 112, 115, 118, 133, 177};
         int[] kroko_jack_soll = new int[0];
@@ -47,37 +47,37 @@ public class Pr1Bsp1Test {
 
     @Test
     public void testMittlereDauerAlbum() {
-        assertEquals(Records.mittlereDauerAlbum(LENGTHS), 59.85, 0.01);
+        assertEquals(at.campus02.iwi.pr1.Records.mittlereDauerAlbum(LENGTHS), 59.85, 0.01);
     }
 
     @Test
     public void testGesamtspieldauer() {
-        assertEquals(Records.gesamtspieldauer(LENGTHS), 9885.29, 0.01);
+        assertEquals(at.campus02.iwi.pr1.Records.gesamtspieldauer(LENGTHS), 9885.29, 0.01);
     }
 
     @Test
     public void testNextSingle() {
-        assertEquals(Records.nextSingle(GENRES, LENGTHS, 0), 64);
-        assertTrue(Records.istSingle(LENGTHS, 64));
+        assertEquals(at.campus02.iwi.pr1.Records.nextSingle(GENRES, LENGTHS, 0), 64);
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 64));
 
-        assertEquals(Records.nextSingle(GENRES, LENGTHS, 3), 4);
-        assertTrue(Records.istSingle(LENGTHS, 4));
+        assertEquals(at.campus02.iwi.pr1.Records.nextSingle(GENRES, LENGTHS, 3), 4);
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 4));
 
-        assertEquals(Records.nextSingle(GENRES, LENGTHS, 4), 7);
-        assertTrue(Records.istSingle(LENGTHS, 7));
+        assertEquals(at.campus02.iwi.pr1.Records.nextSingle(GENRES, LENGTHS, 4), 7);
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 7));
 
-        assertEquals(Records.nextSingle(GENRES, LENGTHS, 8), 9);
-        assertTrue(Records.istSingle(LENGTHS, 9));
+        assertEquals(at.campus02.iwi.pr1.Records.nextSingle(GENRES, LENGTHS, 8), 9);
+        assertTrue(at.campus02.iwi.pr1.Records.istSingle(LENGTHS, 9));
 
         // No more ..
-        assertEquals(Records.nextSingle(GENRES, LENGTHS, 199), -1);
+        assertEquals(at.campus02.iwi.pr1.Records.nextSingle(GENRES, LENGTHS, 199), -1);
     }
 
     @Test
     public void testNextfive() {
-        assertEquals(Records.nextFive(LENGTHS, GENRES, 0, 0), 18.218, 0.001);
-        assertEquals(Records.nextFive(LENGTHS, GENRES, 1,0), 51.80, 0.01);
-        assertEquals(Records.nextFive(LENGTHS, GENRES,2,163), 20.23, 0.01);
+        assertEquals(at.campus02.iwi.pr1.Records.nextFive(LENGTHS, GENRES, 0, 0), 18.218, 0.001);
+        assertEquals(at.campus02.iwi.pr1.Records.nextFive(LENGTHS, GENRES, 1,0), 51.80, 0.01);
+        assertEquals(at.campus02.iwi.pr1.Records.nextFive(LENGTHS, GENRES,2,163), 20.23, 0.01);
     }
 }
 
